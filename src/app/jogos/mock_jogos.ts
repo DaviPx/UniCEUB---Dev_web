e355741fd5c318dd7737ed89ed8cd70154e5cbd0
Carlos Damázio <kaduardobsb95@gmail.com>
2018-05-07 23:30:19 -0300

    Added correct mock_jogos.ts, because I moved it from root to it's correct location.

---
+++ src/app/jogos/mock_jogos.ts
@@ -0,0 +1,20 @@
+import { Jogo } from './jogo';
+
+export const JOGOS: Jogo[]= [
+  {
+    id:1,
+    nome:'Bom de Guerra',
+    votacao: 5,
+    photo:'../../assets/capajogo1.jpg',
+    preco_varejo: 200,
+    preco_medio: 90
+  },
+  {
+    id:2,
+    nome:'Field de Batalha 1',
+    votacao: 4,
+    photo:'../../assets/capajogo2.jpg',
+    preco_varejo:300,
+    preco_medio:150
+  }
+];
