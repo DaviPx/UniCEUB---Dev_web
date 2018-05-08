0977836745879c625f500c01e0d9cc322cea5ab4
Davi Pereira Rocha <30805427+DaviPx@users.noreply.github.com>
2018-05-07 22:58:46 -0300

    Create mock_jogos.ts

---
+++ mock_jogos.ts
@@ -0,0 +1,26 @@
+import { Jogo } from './Jogo';
+
+export const JOGOS: Jogo[]= 
+[
+{
+id:1,
+name:'Bom de Guerra',
+grade: 5,
+photoSrc:'../../assets/capajogo1.jpg'
+
+
+},
+
+{
+id:2,
+name:'Field de Batalha 1',
+grade: 4,
+photoSrc:'../../assets/capajogo2.jpg'
+
+
+}
+
+
+
+];
+
