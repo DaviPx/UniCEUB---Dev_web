44fedd91057bbebead7776d1b502e8f1a7330843
Carlos Damázio <kaduardobsb95@gmail.com>
2018-05-08 00:29:59 -0300

    Added some test and more cleanup.

--- src/app/jogos/mock_jogos.ts
+++ src/app/jogos/mock_jogos.ts
@@ -5,7 +5,7 @@ export const JOGOS: Jogo[]= [
     id:1,
     nome:'Bom de Guerra',
     votacao: 5,
-    photo:'../../assets/capajogo1.jpg',
+    photo:'./assets/capajogo1.jpg',
     preco_varejo: 200,
     preco_medio: 90
   },
@@ -13,7 +13,7 @@ export const JOGOS: Jogo[]= [
     id:2,
     nome:'Field de Batalha 1',
     votacao: 4,
-    photo:'../../assets/capajogo2.jpg',
+    photo:'./assets/capajogo2.jpg',
     preco_varejo:300,
     preco_medio:150
   }
