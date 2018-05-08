52778961d816db0aaa0e4d3c7e419f6d6c323fa8
DaviPx <davipr20@gmail.com>
2018-05-08 15:08:53 -0300

    mudanças no dashboard e assets

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { JOGOS } from '../jogos/mock_jogos';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+
+
+jogos = JOGOS;
+
 
   constructor() { }
 
